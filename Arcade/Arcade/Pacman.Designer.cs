7e4f75e58fccad54c4e0f6ed76f8f25eb7703855
Jacob Frost <36671442+frosty656@users.noreply.github.com>
2018-03-02 10:24:23 -0500

    added title

--- Arcade/Arcade/Pacman.Designer.cs
+++ Arcade/Arcade/Pacman.Designer.cs
@@ -30,6 +30,7 @@
         {
 			this.PushMe = new System.Windows.Forms.Button();
 			this.title = new System.Windows.Forms.Label();
+			this.ClickCounter = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// PushMe
@@ -45,17 +46,28 @@
 			// title
 			// 
 			this.title.AutoSize = true;
-			this.title.Location = new System.Drawing.Point(253, 64);
+			this.title.Location = new System.Drawing.Point(253, 149);
 			this.title.Name = "title";
 			this.title.Size = new System.Drawing.Size(126, 13);
 			this.title.TabIndex = 1;
 			this.title.Text = "You have clicked 0 times";
+			// 
+			// ClickCounter
+			// 
+			this.ClickCounter.AutoSize = true;
+			this.ClickCounter.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.ClickCounter.Location = new System.Drawing.Point(206, 9);
+			this.ClickCounter.Name = "ClickCounter";
+			this.ClickCounter.Size = new System.Drawing.Size(242, 42);
+			this.ClickCounter.TabIndex = 2;
+			this.ClickCounter.Text = "Click Counter";
 			// 
 			// Pacman
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(622, 529);
+			this.Controls.Add(this.ClickCounter);
 			this.Controls.Add(this.title);
 			this.Controls.Add(this.PushMe);
 			this.Name = "Pacman";
@@ -69,5 +81,6 @@
 
 		private System.Windows.Forms.Button PushMe;
 		private System.Windows.Forms.Label title;
+		private System.Windows.Forms.Label ClickCounter;
 	}
 }
