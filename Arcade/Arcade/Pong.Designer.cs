0cede0e441693d3d4fc4f0e14fd6fcb029b0ebdf
Jundels <36671469+Jundels@users.noreply.github.com>
2018-03-06 10:32:52 -0500

    Boi
    Pong Day1

--- Arcade/Arcade/Pong.Designer.cs
+++ Arcade/Arcade/Pong.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Pong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 466);
+            this.Name = "Pong";
             this.Text = "Pong";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
