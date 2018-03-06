25f7d514ad3387b17b290b3af5573219d838fb31
Jundels <36671469+Jundels@users.noreply.github.com>
2018-03-06 13:41:33 -0500

    DAy 1

--- Arcade/Arcade/Pong.Designer.cs
+++ Arcade/Arcade/Pong.Designer.cs
@@ -28,19 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.Player1 = new System.Windows.Forms.PictureBox();
+            this.Player2 = new System.Windows.Forms.PictureBox();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.Player1Score = new System.Windows.Forms.Label();
+            this.Player2Score = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Player1
+            // 
+            this.Player1.BackColor = System.Drawing.SystemColors.Window;
+            this.Player1.Location = new System.Drawing.Point(12, 185);
+            this.Player1.Name = "Player1";
+            this.Player1.Size = new System.Drawing.Size(27, 127);
+            this.Player1.TabIndex = 0;
+            this.Player1.TabStop = false;
+            this.Player1.Click += new System.EventHandler(this.Player1_Click);
+            // 
+            // Player2
+            // 
+            this.Player2.BackColor = System.Drawing.Color.White;
+            this.Player2.Location = new System.Drawing.Point(897, 185);
+            this.Player2.Name = "Player2";
+            this.Player2.Size = new System.Drawing.Size(27, 127);
+            this.Player2.TabIndex = 1;
+            this.Player2.TabStop = false;
+            this.Player2.Click += new System.EventHandler(this.Player2_Click);
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.White;
+            this.ball.Location = new System.Drawing.Point(434, 239);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(27, 26);
+            this.ball.TabIndex = 2;
+            this.ball.TabStop = false;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // Player1Score
+            // 
+            this.Player1Score.AutoSize = true;
+            this.Player1Score.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Player1Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Player1Score.ForeColor = System.Drawing.SystemColors.Window;
+            this.Player1Score.Location = new System.Drawing.Point(132, 57);
+            this.Player1Score.Name = "Player1Score";
+            this.Player1Score.Size = new System.Drawing.Size(34, 25);
+            this.Player1Score.TabIndex = 3;
+            this.Player1Score.Text = "00";
+            // 
+            // Player2Score
+            // 
+            this.Player2Score.AutoSize = true;
+            this.Player2Score.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Player2Score.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Player2Score.ForeColor = System.Drawing.SystemColors.Window;
+            this.Player2Score.Location = new System.Drawing.Point(746, 57);
+            this.Player2Score.Name = "Player2Score";
+            this.Player2Score.Size = new System.Drawing.Size(34, 25);
+            this.Player2Score.TabIndex = 4;
+            this.Player2Score.Text = "00";
             // 
             // Pong
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(969, 466);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(935, 566);
+            this.Controls.Add(this.Player2Score);
+            this.Controls.Add(this.Player1Score);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.Player2);
+            this.Controls.Add(this.Player1);
             this.Name = "Pong";
             this.Text = "Pong";
+            this.Load += new System.EventHandler(this.Pong_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisdown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisup);
+            ((System.ComponentModel.ISupportInitialize)(this.Player1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox Player1;
+        private System.Windows.Forms.PictureBox Player2;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.Label Player1Score;
+        private System.Windows.Forms.Label Player2Score;
     }
 }
--- Arcade/Arcade/Pong.cs
+++ Arcade/Arcade/Pong.cs
@@ -12,9 +12,128 @@ namespace Arcade
 {
     public partial class Pong : Form
     {
+
+        bool GoUp1;//Bool to determine whether the player wants to go up or not
+        bool GoUp2;//Bool top determine whether player2 wants to go up or not
+        bool GoDown1;//Bool to determine whether the player wants to go down or not
+        bool GoDown2;//Bool to determine whether the player2 wants to go up or not
+        int speed = 5;
+        int ballx = 5;//Horizontal X xpeed value for the ball object
+        int bally = 5;//Vertical Y speed value for the ball object
+        int Score1 = 0;//Scpre for player 1
+        int Score2 = 0;//Score for player 2
+
         public Pong()
         {
             InitializeComponent();
+        }
+
+        private void Player2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Pong_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Player1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void keyisdown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)//Changes the up Bool to true if the player1 presses the up button
+            {
+                GoUp1 = true;
+            }
+            if (e.KeyCode == Keys.Up )//Changes the up Bool to true if the player2 presses the up button
+            {
+                GoUp2 = true;
+            }
+            if (e.KeyCode == Keys.Down)//Changes the down bool to true if the player1 presses the down button
+            {
+                GoDown1 = true;
+            }
+            if (e.KeyCode == Keys.S)//Changes the down bool to true if the player2 presses the S button
+            {
+                GoDown2 = true;
+            }
+        }
+
+        private void keyisup(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Up)//Changes the up Bool to false if the player presses the up button
+            {
+                GoUp2 = false;
+            }
+            if (e.KeyCode == Keys.W)//Changes the up Bool to false if player1 presses the W key
+            {
+                GoUp1 = false;
+            }
+            if (e.KeyCode == Keys.Down )//Changes the down bool to false if the player presses the down button
+            {
+                GoDown2 = false;
+            }
+            if (e.KeyCode == Keys.S)//Changes the down bool to false if the player presses the down button
+            {
+                GoDown1 = false;
+            }
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            Player1Score.Text = "" + Score1;//shows the score for player 1
+            Player2Score.Text = "" + Score2;//shows the score for player 2
+
+            ball.Top -= bally;//Assigns the ball Top to bally integer
+            ball.Left -= ballx;//Assigns the ball Left to the ballx integer
+
+            if (ball.Left < 0)//Checks the score if the ball gets past the left through player one
+            {
+                ball.Left = 434;//Resets the ball to the middle of the screen
+                ballx = -ballx;//Changes the ball direction
+                ballx -= 2; // increases the speed of the ball
+                Score2++;
+            }
+
+            if (ball.Left + ball.Width > ClientSize.Width)//Checks the score if the ball gets past the right through player 2
+            {
+                ball.Left = 434;//Resets the ball to the middle of the screen
+                ballx = -ballx;//Changes the direction of the ball
+                ballx += 2; //Increases the speed of the ball
+                Score1++;//adds one to player1 score
+            }
+
+            if (ball.Top < 0 | ball.Top + ball.Height > ClientSize.Height)//Sets the boundary for the top and bottom of the screen
+            {
+                bally = -bally;//Reverses the speed of the ball so that the ball stays within the screen
+            }
+
+            if (ball.Bounds.IntersectsWith(Player1.Bounds) | ball.Bounds.IntersectsWith(Player2.Bounds))//if the ball hits the player1 opr player 2
+            {
+                ballx = -ballx;//The ball bounces in the other direction
+            }
+
+            if (GoUp1 == true & Player1.Top > 0)//allows player1 to move up
+            {
+                Player1.Top -= 8;
+            }
+            if (GoUp2 == true & Player2.Top > 0)//allows player2 to move up
+            {
+                Player2.Top -= 8;
+            }
+
+            if (GoDown1 == true & Player1.Top < 455)//allows player1 to move up
+            {
+                Player1.Top += 8;
+            } 
+            if (GoDown2 == true & Player2.Top < 455)//allows player2 to move up
+            {
+                Player2.Top += 8;
+            }
         }
     }
 }
