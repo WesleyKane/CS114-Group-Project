3625e5c9df03fa62980457fa059a9272eb0a25c1
Jacob Frost <36671442+frosty656@users.noreply.github.com>
2018-03-27 15:31:35 -0400

    Added game

--- Arcade/Arcade/Properties/Resources.Designer.cs
+++ Arcade/Arcade/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Arcade.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -86,6 +86,36 @@ namespace Arcade.Properties {
         internal static System.IO.UnmanagedMemoryStream astHit {
             get {
                 return ResourceManager.GetStream("astHit", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap BattleShipBackground {
+            get {
+                object obj = ResourceManager.GetObject("BattleShipBackground", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap BattleShipFireIcon {
+            get {
+                object obj = ResourceManager.GetObject("BattleShipFireIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap BattleShipMissIcon {
+            get {
+                object obj = ResourceManager.GetObject("BattleShipMissIcon", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
         
