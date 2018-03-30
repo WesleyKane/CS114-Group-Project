b72ea1568b9d9ca771c5bfa16cb1ac3b5dccd83e
Jundels <36671469+Jundels@users.noreply.github.com>
2018-03-30 11:14:25 -0400

    Pong w/ sound effects
    pong with sounds

--- Arcade/Arcade/Pong.cs
+++ Arcade/Arcade/Pong.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Media;
 
+
 namespace Arcade
 {
     public partial class Pong : Form
@@ -28,21 +29,29 @@ namespace Arcade
         int B2x = 15;
         int B2Position = 40;
         int B1Position = 40;
+
+        System.Media.SoundPlayer victory = new System.Media.SoundPlayer(Properties.Resources.GuilesThemeI);//Sound for the winner
+        System.Media.SoundPlayer bounce = new System.Media.SoundPlayer(Properties.Resources.Bounce);//Sound effects for the ball hitting an object
+        System.Media.SoundPlayer Laser = new System.Media.SoundPlayer(Properties.Resources.Laser_Shoot1);//Sound effect for shooting
+        System.Media.SoundPlayer LoseLife = new System.Media.SoundPlayer(Properties.Resources.LoseLife);//sound effect for when a players paddle get shot
+
         public Pong()
         {
             InitializeComponent();
             GameTimer.Stop();//Sets it so that the game doesnt start until one of the players moves their bars
-            
+
         }
+
+
 
         private void Player2_Click(object sender, EventArgs e)
         {
-            
+
         }
 
         private void Pong_Load(object sender, EventArgs e)
         {
-           
+
         }
 
         private void Player1_Click(object sender, EventArgs e)
@@ -73,7 +82,7 @@ namespace Arcade
                 GoDown1 = false;
                 GameTimer.Start();
             }
-           
+
         }
 
         private void keyisdown(object sender, KeyEventArgs e)
@@ -91,9 +100,10 @@ namespace Arcade
             if (e.KeyCode == Keys.D)
             {
                 shoot1 = true;
+                Laser.Play();
                 GameTimer.Start();
             }
-            if (e.KeyCode == Keys.Up )//Changes the up Bool to true if the player2 presses the up button
+            if (e.KeyCode == Keys.Up)//Changes the up Bool to true if the player2 presses the up button
             {
                 GoUp2 = true;
                 GameTimer.Start();
@@ -106,54 +116,63 @@ namespace Arcade
             if (e.KeyCode == Keys.Left)
             {
                 shoot2 = true;
+                Laser.Play();
                 GameTimer.Start();
             }
         }
-        
+
+
+
         private void GameTimer_Tick(object sender, EventArgs e)
         {
+
+
+
             Player1Score.Text = "" + Score1;//shows the score for player 1
             Player2Score.Text = "" + Score2;//shows the score for player 2
 
             //Allows the ball to move
             ball.Top -= bally;//Assigns the ball Top to bally integer
             ball.Left -= ballx;//Assigns the ball Left to the ballx integer
-            
+
             //Allow player 1 to shoot a block at player2
             if (shoot1 == false)
             {
-                B1.Location = new Point(Player1.Location.X ,Player1.Location.Y + B1Position);
+                B1.Location = new Point(Player1.Location.X, Player1.Location.Y + B1Position);
             }
-           
+
             if (shoot1 == true)
             {
                 B1.Left += B1x;
             }
             if (B1.Left + B1.Width > ClientSize.Width)
             {
-                B1.Location = new Point(Player1.Location.X , Player1.Location.Y + B1Position);
+                B1.Location = new Point(Player1.Location.X, Player1.Location.Y + B1Position);
                 shoot1 = false;
             }
             if (B1.Bounds.IntersectsWith(Player2.Bounds))
             {
                 Player2.Height /= 2;
                 B2.Height /= 2;
-                B1.Location = new Point(Player1.Location.X , Player1.Location.Y + B1Position);
+                B1.Location = new Point(Player1.Location.X, Player1.Location.Y + B1Position);
                 B2Position /= 2;
+                LoseLife.Play();
                 shoot1 = false;
             }
-            
+
             //Allows player2 to shoot at player1
             if (shoot2 == false)
             {
                 B2.Location = new Point(Player2.Location.X, Player2.Location.Y + B2Position);
+
             }
 
             if (shoot2 == true)
             {
+
                 B2.Left -= B2x;
             }
-            if (B2.Left<0)
+            if (B2.Left < 0)
             {
                 B2.Location = new Point(Player2.Location.X, Player2.Location.Y + B2Position);
                 shoot2 = false;
@@ -164,6 +183,7 @@ namespace Arcade
                 B2.Location = new Point(Player2.Location.X, Player2.Location.Y + B2Position);
                 B1Position /= 2;
                 B1.Height /= 2;
+                LoseLife.Play();
                 shoot2 = false;
             }
 
@@ -171,11 +191,12 @@ namespace Arcade
             {
                 int CenterH = ClientSize.Height / 2;
                 int CenterW = ClientSize.Width / 2;
+                Score2++;//adds one to player2 score
                 ball.Left = CenterW;//Resets the ball to the middle of the screen
                 ball.Top = CenterH;//Resets the ball to the center of the screen height wise
                 ballx = -ballx;//Changes the ball direction
-                //ballx -= 1; // increases the speed of the ball
-                Score2++;//adds one to player2 score
+                               //ballx -= 1; // increases the speed of the ball
+
                 GameTimer.Stop();//Pauses the game whenever a player scores
             }
 
@@ -183,22 +204,33 @@ namespace Arcade
             {
                 int CenterH = ClientSize.Height / 2;
                 int CenterW = ClientSize.Width / 2;
+                Score1++;//adds one to player1 score
                 ball.Left = CenterW;//Resets the ball to the middle of the screen width wise
                 ball.Top = CenterH;//Resets the ball to the center of the screen height wise
                 ballx = -ballx;//Changes the direction of the ball
-                //ballx += 1; //Increases the speed of the ball
-                Score1++;//adds one to player1 score
+                               //ballx += 1; //Increases the speed of the ball
+
                 GameTimer.Stop();//Pauses the game whenever a player scores
             }
 
             if (ball.Top < 0 | ball.Top + ball.Height > ClientSize.Height)//Sets the boundary for the top and bottom of the screen
             {
                 bally = -bally;//Reverses the speed of the ball so that the ball stays within the screen
+                bounce.Play();
             }
 
             if (ball.Bounds.IntersectsWith(Player1.Bounds) | ball.Bounds.IntersectsWith(Player2.Bounds) | ball.Bounds.IntersectsWith(B1.Bounds) | ball.Bounds.IntersectsWith(B2.Bounds))//if the ball hits the player1 or player 2
             {
                 ballx = -ballx;//The ball bounces in the other direction
+                bounce.Play();
+            }
+            if (B1.Bounds.IntersectsWith(B2.Bounds))
+            {
+                B1.Location = new Point(Player1.Location.X, Player1.Location.Y + B1Position);
+                B2.Location = new Point(Player2.Location.X, Player2.Location.Y + B2Position);
+                shoot1 = false;
+                shoot2 = false;
+                LoseLife.Play();
             }
 
             if (GoUp1 == true & Player1.Top > 0)//allows player1 to move up
@@ -214,7 +246,7 @@ namespace Arcade
 
             if (GoDown2 == true & Player1.Top < Player1.Bottom)//allows player1 to move down
             {
-                
+
                 Player2.Top += 8;
                 GameTimer.Start();
             }
@@ -224,15 +256,20 @@ namespace Arcade
                 GameTimer.Start();
             }
 
-            if (Score1 == 10)//IF player 1 scores 10 points then player1 wins
+            if (Score1 == 1)//IF player 1 scores 10 points then player1 wins
             {
+
                 GameTimer.Stop();
+                victory.Play();
                 MessageBox.Show("Player 1 wins!");
+                victory.Stop();
             }
-            if (Score2 == 10)//If player 2 wins then player 2 wins
+            if (Score2 == 1)//If player 2 wins then player 2 wins
             {
                 GameTimer.Stop();
+                victory.Play();
                 MessageBox.Show("Player 2 wins!");
+                victory.Stop();
             }
         }
 
@@ -245,6 +282,6 @@ namespace Arcade
         {
 
         }
-        
+
     }
 }
